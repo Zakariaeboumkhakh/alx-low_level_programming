6962c01222c3cb37fbc25012f83f4c882e447aa4
zakariae boumkhakh <zakariaboumkhakh@gmail.com>
2023-04-13 17:12:42 -0700

    hhh

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-
 *malloc_checked - Terminates process with status 98 if malloc fails.
 *@b: Amount of memory to allocate.
 *Return: Pointer to the allocated memory.
 */
-void *malloc_checked(unsigned int c)
+void *malloc_checked(unsigned int)
 {
 	void *ret;
 
