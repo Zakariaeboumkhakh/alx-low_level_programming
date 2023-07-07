f9a886b43c314104b85debed0fd686db2c3aa376
zakariaboumkhakh <zakariaboumkhakh14@gmail.com>
2023-07-07 01:10:01 +0100

    pushing

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
 /**
- *  * get_bit - Returns the value of a bit at a given index
- *   *
- *    * @n: number to search bit in
- *     * @index: index of the bit to return
- *      *
- *       * Return: Value of bit at given index or -1 if an error occured
- *        */
-int get_bit(unsigned long int n, unsigned int index)
+ *  * set_bit - Sets the value of a bit at a given index to 1
+ *   * @n: pointer to the number to modify
+ *    * @index: index of the bit to set, starting from 0
+ *     *
+ *      * Return: 1 if it worked, or -1 if an error occurred
+ *       */
+int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
-	return ((n >> index) & 1);
+	*n |= (1 << index);
+
+	return (1);
 }
