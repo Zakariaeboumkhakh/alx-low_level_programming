210a941844f2d55af6cd0d93712a8d872b24cb1a
zakariaboumkhakh <zakariaboumkhakh14@gmail.com>
2023-07-06 18:17:57 -0700

    pushing

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,10 +1,10 @@
 /**
- *  * set_bit - Sets the value of a bit at a given index to 1
- *   * @n: pointer to the number to modify
- *    * @index: index of the bit to set, starting from 0
- *     *
- *      * Return: 1 if it worked, or -1 if an error occurred
- *       */
+ * set_bit - Sets the value of a bit at a given index to 1
+ * @n: pointer to the number to modify
+ * @index: index of the bit to set, starting from 0
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > (sizeof(unsigned long int) * 8 - 1))
